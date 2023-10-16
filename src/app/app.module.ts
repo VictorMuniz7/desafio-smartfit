cbc0dae5447ec1a0ec9afabf8d6634a60d65d32a
Kairuus <kingkairuus@gmail.com>
2023-10-16 14:37:46 -0300

    creating header, form and legend components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FormComponent } from './components/form/form.component';
+import { LegendComponent } from './components/legend/legend.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FormComponent,
+    LegendComponent
   ],
   imports: [
     BrowserModule,
